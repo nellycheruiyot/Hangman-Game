e0491d7ea42fb3300d90822c3ed378843cad6f15
Nelly Cheruiyot <nellycheruiyot@gmail.com>
2017-12-03 22:29:24 +0300

    Implement while loop and add method to print hangman image

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,15 +7,108 @@ public class App {
     Hangman hangman = new Hangman();
     String word = hangman.getWord();
     String answer = hangman.getAnswer();
-    while (!word.equals(answer)) {
+    int count = hangman.getWrongLetterCount();
+    boolean hang = hangman.getHang();
+
+    while (count<7 && !word.equals(answer)) {
       System.out.println("Guess a letter: ");
       String letter = myConsole.readLine();
       hangman.playGame(letter);
       answer = hangman.getAnswer();
-      System.out.println(word);
+      count = hangman.getWrongLetterCount();
+      hang = hangman.getHang();
+      // System.out.println(word);
       System.out.println(answer);
-
+      if (hang) {
+        printHangman(count, word);
+      }
+    }
+    if (word.equals(answer)) {
+      System.out.println("Congratulations! You Won!");
     }
 
+  }
+  public static void printHangman(int count, String word) {
+    if (count == 1) {
+			System.out.println("Wrong guess, try again");
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println("___|___");
+			System.out.println();
+		}
+		if (count == 2) {
+			System.out.println("Wrong guess, try again");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("___|___");
+		}
+		if (count == 3) {
+			System.out.println("Wrong guess, try again");
+			System.out.println("   ____________");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   | ");
+			System.out.println("___|___");
+		}
+		if (count == 4) {
+			System.out.println("Wrong guess, try again");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("___|___");
+		}
+		if (count == 5) {
+			System.out.println("Wrong guess, try again");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |           |");
+			System.out.println("   |           |");
+			System.out.println("   |");
+			System.out.println("___|___");
+		}
+		if (count == 6) {
+			System.out.println("Wrong guess, try again");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |           |");
+			System.out.println("   |           |");
+			System.out.println("   |          / \\ ");
+			System.out.println("___|___      /   \\");
+		}
+		if (count == 7) {
+			System.out.println("GAME OVER!");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |          _|_");
+			System.out.println("   |         / | \\");
+			System.out.println("   |          / \\ ");
+			System.out.println("___|___      /   \\");
+			System.out.println("GAME OVER! The word was " + word);
+		}
   }
 }

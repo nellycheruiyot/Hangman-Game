7af1bc2a20a02cc048dc3f25f184136e2deb321c
Nelly Cheruiyot <nellycheruiyot@gmail.com>
2017-12-03 19:18:10 +0300

    Add constructor

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,3 +1,19 @@
+import java.util.*;
+
 public class Hangman {
+  private static String[] words = {"nelly", "money", "java", "laptop", "bottle", "book"};
+  private String word;
+  private String answer;
+  private List<String> playerLetters = new ArrayList<String>();
+  private Boolean hang;
+  private int wrongLetterCount;
+
+  public Hangman() {
+
+  }
+
+  /*public void playGame() {
+
+  }*/
 
 }

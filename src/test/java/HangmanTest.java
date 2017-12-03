9471e0f129d25164571f0edbba87b7a3eefe7dd3
Nelly Cheruiyot <nellycheruiyot@gmail.com>
2017-12-03 19:19:08 +0300

    Add JUnit test for constructorr

--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -3,4 +3,10 @@ import static org.junit.Assert.*;
 
 public class HangmanTest {
 
+
+  @Test
+  public void newHangman_instantiatesCorrectly() {
+    Hangman testHangman = new Hangman();
+    assertEquals(true, testHangman instanceof Hangman);
+  }
 }
